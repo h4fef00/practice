59e414a6bc4cdd427a912539db3f02ab843277d6
h4fef <afef.hassani@fiven.eu>
2023-11-04 12:28:40 +0100

    aggiunto progetto in ts, aggiunta sidebar in js

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,7 +1,19 @@
+// import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+// import Home from "./components/home/Home";
+// import Login from "./components/login/Form";
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      {/* <Routes>
+        <Route path="/" element={<Navbar />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes> */}
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
