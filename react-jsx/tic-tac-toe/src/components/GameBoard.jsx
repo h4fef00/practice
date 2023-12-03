7c1ccdd7ce83f485a6598263ce87c84fb6986b4a
h4fef <afef.hassani@fiven.eu>
2023-12-03 20:40:06 +0100

    ok

--- react-jsx/tic-tac-toe/src/components/GameBoard.jsx
+++ react-jsx/tic-tac-toe/src/components/GameBoard.jsx
@@ -5,17 +5,19 @@ const initialGameBoard = [
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard() {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArrays) => [...innerArrays]),
-      ];
-      updatedBoard[rowIndex][colIndex] = "X";
-      return updatedBoard;
-    });
-  }
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArrays) => [...innerArrays]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
+
+  //   onSelectSquare();
+  // }
   return (
     <ol id="game-board">
       {gameBoard.map((row, index) => (
